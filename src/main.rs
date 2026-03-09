dd63e18d6cc1aed81f9f363952d7378cda597a76
Nathaniel Parah <nathanielparah579@gmail.com>
2026-03-09 09:49:52 +0100

    created a search functionality to check if the request matches the path
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,24 @@ fn main() {
 
 fn handle_request(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 Ok\r\n\r\n";
+        let contents = fs::read_to_string("natty.html").unwrap();
+        let length = contents.len();
 
-    let status_line = "HTTP/1.1 200 Ok\r\n\r\n";
-    let contents = fs::read_to_string("web.html").unwrap();
-    let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        // some other request
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
 
-    stream.write_all(response.as_bytes()).unwrap();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
