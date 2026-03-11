609dbef426fde6a0f8aafca4b4ab1e5ae90b239a
Nathaniel Parah <nathanielparah579@gmail.com>
2026-03-11 01:13:03 +0100

    Added the number of request to be received before shutdown
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,15 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").expect("failed to bind");
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_request(stream)
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_request(mut stream: TcpStream) {
